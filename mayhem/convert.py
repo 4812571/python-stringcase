7e3fbc4486a6dd626a9017df456148e94e9d395f
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-08 08:24:39 -0400

    Update convert.py

--- mayhem/convert.py
+++ mayhem/convert.py
@@ -12,38 +12,36 @@ def TestOneInput(data):
     num = fdp.ConsumeIntInRange(0, 10)
     str = fdp.ConsumeString(128)
 
-    if num == 1:
+    if num == 0:
         stringcase.camelcase(str)
     elif num == 1:
         stringcase.capitalcase(str)
-    elif num == 1:
+    elif num == 2:
         stringcase.constcase(str)
-    elif num == 1:
+    elif num == 3:
         stringcase.lowercase(str)
-    elif num == 1:
+    elif num == 4:
         stringcase.pascalcase(str)
-    elif num == 1:
+    elif num == 5:
         stringcase.pathcase(str)
-    elif num == 1:
+    elif num == 6:
         stringcase.sentencecase(str)
-    elif num == 1:
+    elif num == 7:
         stringcase.snakecase(str)
-    elif num == 1:
+    elif num == 8:
         stringcase.spinalcase(str)
-    elif num == 1:
+    elif num == 9:
         stringcase.titlecase(str)
-    elif num == 1:
+    elif num == 10:
         stringcase.trimcase(str)
-    elif num == 1:
+    elif num == 11:
         stringcase.uppercase(str)
-    elif num == 1:
+    elif num == 12:
         stringcase.alphanumcase(str)
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
     atheris.Fuzz()
-
-print(__name__)
 
 if __name__ == "__main__":
     main()
