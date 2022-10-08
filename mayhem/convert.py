04000ca0ec33b3d4aa5e099fe03b211bcd02e6e8
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-08 07:45:40 -0400

    integrated mayhem

---
+++ mayhem/convert.py
@@ -0,0 +1,49 @@
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    import stringcase
+
+@atheris.instrument_func
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+
+    num = fdp.ConsumeIntInRange(0, 10)
+    str = fdp.ConsumeString(128)
+
+    if num == 1:
+        stringcase.camelcase(str)
+    elif num == 1:
+        stringcase.capitalcase(str)
+    elif num == 1:
+        stringcase.constcase(str)
+    elif num == 1:
+        stringcase.lowercase(str)
+    elif num == 1:
+        stringcase.pascalcase(str)
+    elif num == 1:
+        stringcase.pathcase(str)
+    elif num == 1:
+        stringcase.sentencecase(str)
+    elif num == 1:
+        stringcase.snakecase(str)
+    elif num == 1:
+        stringcase.spinalcase(str)
+    elif num == 1:
+        stringcase.titlecase(str)
+    elif num == 1:
+        stringcase.trimcase(str)
+    elif num == 1:
+        stringcase.uppercase(str)
+    elif num == 1:
+        stringcase.alphanumcase(str)
+
+def main():
+    print('main')
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+print(__name__)
+
+if __name__ == "__main__":
+    main()
